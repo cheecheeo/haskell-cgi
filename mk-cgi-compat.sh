1b83f4e2c5018df16be0840650279083483439da
Peter Simons <simons@cryp.to>
2019-01-25 17:27:57 +0100

    mk-cgi-compat.sh: drop obsolete script

--- mk-cgi-compat.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-sed -i.orig -e 's/^name:  *cgi/name: cgi-compat/' -e 's/Network\.CGI,/Network.NewCGI,/' -e 's/Build-depends:\(.*\)/Build-depends:\1, fps/' cgi.cabal
-sed -e 's/^module Network\.CGI/module Network.NewCGI/' < src/Network/CGI.hs > src/Network/NewCGI.hs
